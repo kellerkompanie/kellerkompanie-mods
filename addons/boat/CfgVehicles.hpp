14b75008382031ee8d95417d95a5b9b42da96875
Schwaggot <tom.ryan@posteo.de>
2018-09-15 14:21:56 +0200

    added tanoa boat

--- addons/boat/CfgVehicles.hpp
+++ addons/boat/CfgVehicles.hpp
@@ -1,9 +1,17 @@
 #include "script_component.hpp"
 
+#define ENGINEPOWER 100
+#define MAXSPEED 65
+
 class CfgVehicles {
 	class Rubber_duck_base_F;
 	class B_Boat_Transport_01_F: Rubber_duck_base_F {
-		enginePower = 100;
-		maxSpeed = 65;
+		enginePower = ENGINEPOWER;
+		maxSpeed = MAXSPEED;
+	};
+
+	class B_T_Boat_Transport_01_F: Rubber_duck_base_F {
+		enginePower = ENGINEPOWER;
+		maxSpeed = MAXSPEED;
 	};
 };
