730fb07b84e8eba3f943880535a4a7e532b3f723
Schwaggot <tom.ryan@posteo.de>
2018-09-15 14:23:27 +0200

    adjusted script macro

--- addons/boat/script_component.hpp
+++ addons/boat/script_component.hpp
@@ -1,17 +1,17 @@
-#define COMPONENT ai
-#define COMPONENT_BEAUTIFIED AI
+#define COMPONENT boat
+#define COMPONENT_BEAUTIFIED BOAT
 #include "\x\keko\addons\main\script_mod.hpp"
 
 // #define DEBUG_MODE_FULL
 // #define DISABLE_COMPILE_CACHE
 // #define ENABLE_PERFORMANCE_COUNTERS
 
-#ifdef DEBUG_ENABLED_AI
+#ifdef DEBUG_ENABLED_BOAT
     #define DEBUG_MODE_FULL
 #endif
 
-#ifdef DEBUG_SETTINGS_AI
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_AI
+#ifdef DEBUG_SETTINGS_BOAT
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_BOAT
 #endif
 
 #include "\x\keko\addons\main\script_macros.hpp"
