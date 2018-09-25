4a95b0a29a3733b3f5b252922613b801b3038443
Schwaggot <tom.ryan@posteo.de>
2018-09-25 13:59:50 +0200

    first version of clean actionmenu

---
+++ addons/clean_actionmenu/CfgActions.hpp
@@ -0,0 +1,34 @@
+class CfgActions {
+    // to pistol
+    class None;
+	class HandGunOn: None {
+        show = 0;
+	};
+    // to rifle
+    class HandGunOff: None {
+        show = 0;
+    };
+
+    // to launcher, (also used for binoculars?)
+    class SwitchWeapon: None {
+        show = 0;
+    };
+    class SwitchMagazine: SwitchWeapon {
+        show = 0;
+    };
+    // no idea, probably unused
+    class HideWeapon: SwitchWeapon {
+        show = 0;
+    };
+
+
+	class LoadMagazine: None {
+        show = 0;
+	};
+	class LoadEmptyMagazine: LoadMagazine {
+		show = 0;
+	};
+	class LoadOtherMagazine: LoadMagazine {
+		show = 0;
+	};
+};
