cd97175a17304b0bc458ffa5a93c1bed8cc43bda
Schwaggot <tom.ryan@posteo.de>
2018-10-01 23:28:33 +0200

    minor changes trying to fix build error

--- addons/boat/config.cpp
+++ addons/boat/config.cpp
@@ -4,6 +4,7 @@ class CfgPatches {
     class ADDON {
         name = COMPONENT_NAME;
         units[] = {};
+		weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
         requiredAddons[] = {
             "cba_main"};
