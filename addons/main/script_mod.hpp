db594a1803ee18163ebb9940a72cd3cf1d7ca763
Schwaggot <tom.ryan@posteo.de>
2018-10-01 17:56:59 +0200

    updated version in macro to 2.0.1.2

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -5,8 +5,8 @@
 
 #define MAJOR 2
 #define MINOR 0
-#define PATCHLVL 0
-#define BUILD 1
+#define PATCHLVL 1
+#define BUILD 2
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
