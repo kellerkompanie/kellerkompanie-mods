2c0f0cd32dda0fe09afb19868091e7c1760597be
Schwaggot <tom.ryan@posteo.de>
2018-10-01 23:28:33 +0200

    minor changes trying to fix build error

--- addons/boat/CfgVehicles.hpp
+++ addons/boat/CfgVehicles.hpp
@@ -1,17 +1,14 @@
 #include "script_component.hpp"
 
-#define ENGINEPOWER 100
-#define MAXSPEED 65
-
 class CfgVehicles {
 	class Rubber_duck_base_F;
 	class B_Boat_Transport_01_F: Rubber_duck_base_F {
-		enginePower = ENGINEPOWER;
-		maxSpeed = MAXSPEED;
+		enginePower = 100;
+		maxSpeed = 65;
 	};
 
 	class B_T_Boat_Transport_01_F: Rubber_duck_base_F {
-		enginePower = ENGINEPOWER;
-		maxSpeed = MAXSPEED;
+		enginePower = 100;
+		maxSpeed = 65;
 	};
 };
