6562c571de12c71d48d405cf6818e329c2015bec
Schwaggot <tom.ryan@posteo.de>
2018-10-06 12:50:56 +0200

    tweaked power of assault boat

--- addons/boat/CfgVehicles.hpp
+++ addons/boat/CfgVehicles.hpp
@@ -1,12 +1,12 @@
 class CfgVehicles {
 	class Rubber_duck_base_F;
 	class B_Boat_Transport_01_F: Rubber_duck_base_F {
-		enginePower = 100;
+		enginePower = 50;
 		maxSpeed = 65;
 	};
 
 	class B_T_Boat_Transport_01_F: Rubber_duck_base_F {
-		enginePower = 100;
+		enginePower = 50;
 		maxSpeed = 65;
 	};
 
