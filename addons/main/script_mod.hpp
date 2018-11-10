319943c53206173188ad8ee1f9ad841d95e054c4
Schwaggot <tom.ryan@posteo.de>
2018-11-10 13:20:38 +0100

    v2.5.0

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,9 +4,9 @@
 #define PREFIX keko
 
 #define MAJOR 2
-#define MINOR 4
-#define PATCHLVL 5
-#define BUILD 27
+#define MINOR 5
+#define PATCHLVL 0
+#define BUILD 28
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
