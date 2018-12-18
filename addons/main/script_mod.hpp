31ce2fcefffe6f3288224655bdc47efa0e5ca998
Schwaggot <tom.ryan@posteo.de>
2018-12-18 16:41:11 +0100

    v2.6.3

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -5,8 +5,8 @@
 
 #define MAJOR 2
 #define MINOR 6
-#define PATCHLVL 2
-#define BUILD 35
+#define PATCHLVL 3
+#define BUILD 36
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
