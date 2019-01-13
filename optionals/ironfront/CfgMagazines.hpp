2e620352e848e6de75743b9db0926abfc9192d68
Schwaggot <tom.ryan@posteo.de>
2019-01-13 19:16:42 +0100

    added 75, 150 and 250 round mags for MG34 and MG42

---
+++ optionals/ironfront/CfgMagazines.hpp
@@ -0,0 +1,39 @@
+class CfgMagazines {
+
+	class LIB_50Rnd_792x57;
+	class GVAR(LIB_75Rnd_792x57): LIB_50Rnd_792x57 {
+		descriptionShort = "75 Rounds Magazine";
+		displayName = "75 rounds Gurttrommel";
+		count = 75;
+	};
+
+	class GVAR(LIB_150Rnd_792x57): LIB_50Rnd_792x57 {
+		descriptionShort = "150 Rounds Magazine";
+		displayName = "150 rounds Gurttrommel";
+		count = 150;
+	};
+
+	class GVAR(LIB_250Rnd_792x57): LIB_50Rnd_792x57 {
+		descriptionShort = "250 Rounds Magazine";
+		displayName = "250 rounds Gurttrommel";
+		count = 250;
+	};
+
+	class fow_50Rnd_792x57;
+	class GVAR(FOW_75Rnd_792x57): fow_50Rnd_792x57 {
+		displayName = "75Rnd 7.92x57";
+		displayNameShort = "75Rnd 7.92x57";
+		count = 75;
+	};
+	class GVAR(FOW_150Rnd_792x57): fow_50Rnd_792x57 {
+		displayName = "150Rnd 7.92x57";
+		displayNameShort = "150Rnd 7.92x57";
+		count = 150;
+	};
+	class GVAR(FOW_250Rnd_792x57): fow_50Rnd_792x57 {
+		displayName = "250Rnd 7.92x57";
+		displayNameShort = "250Rnd 7.92x57";
+		count = 250;
+	};
+
+};
