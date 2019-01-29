2808111e1d4b49f1348a8b310172bb0faf9fdf19
Schwaggot <tom.ryan@posteo.de>
2019-01-30 00:08:23 +0100

    added modded tigris for Gunk

---
+++ optionals/gunks_tigris/config.cpp
@@ -0,0 +1,19 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+	class ADDON {
+		name = COMPONENT_NAME;
+		units[]= {};
+		weapons[] = {};
+		requiredVersion= REQUIRED_VERSION;
+		requiredAddons[]= {
+            "A3_Weapons_F"
+		};
+		author = ECSTRING(common,KEKOTeam);
+        authors[] = {"Schwaggot"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+	};
+};
+
+#include "CfgWeapons.hpp"
