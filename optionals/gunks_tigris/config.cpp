2b9896a5592c21ec7e76a1dc1fb7e013c211e85d
Schwaggot <tom.ryan@posteo.de>
2019-01-30 21:39:46 +0100

    changed more parameters to Gunk's wishes

--- optionals/gunks_tigris/config.cpp
+++ optionals/gunks_tigris/config.cpp
@@ -4,7 +4,9 @@ class CfgPatches {
 	class ADDON {
 		name = COMPONENT_NAME;
 		units[]= {};
-		weapons[] = {};
+		weapons[] = {
+			"missiles_titan_AA"
+		};
 		requiredVersion= REQUIRED_VERSION;
 		requiredAddons[]= {
             "A3_Weapons_F"
