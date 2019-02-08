30d111ee29971471301d747fc4e218f3e62e296b
Schwaggot <tom.ryan@posteo.de>
2019-02-08 23:30:07 +0100

    added cTab fix based on Bamse

---
+++ addons/ctab_fix/CfgFunctions.hpp
@@ -0,0 +1,13 @@
+class CfgFunctions
+{
+	class cTab
+	{
+		class Functions
+		{
+			class drawHook
+			{
+				file = QPATHTOF(functions\fnc_drawHook.sqf);
+			};
+		};
+	};
+};
---
+++ addons/ctab_fix/README.md
@@ -0,0 +1,2 @@
+## keko_ctab_fix
+Based on cTab fix by bamse https://steamcommunity.com/sharedfiles/filedetails/?id=973255022&searchtext=bamse
