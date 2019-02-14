af66cac6501743e34b989a0786d7de68daf4805c
Schwaggot <tom.ryan@posteo.de>
2019-02-14 11:54:57 +0100

    added noactionmenu from ACE to avoid optionals

---
+++ addons/noactionmenu/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT noactionmenu
+#define COMPONENT_BEAUTIFIED No Action Menu
+#include "\x\keko\addons\main\script_mod.hpp"
+
+#ifdef DEBUG_ENABLED_NOACTIONMENU
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_ENABLED_NOACTIONMENU
+    #define DEBUG_SETTINGS DEBUG_ENABLED_NOACTIONMENU
+#endif
+
+#include "\x\keko\addons\main\script_macros.hpp"
