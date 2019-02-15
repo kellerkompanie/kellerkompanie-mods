3e6b2bf62aafa42644d92eb2128a59c860f170f4
Schwaggot <tom.ryan@posteo.de>
2019-02-15 14:53:23 +0100

    added sci-fi addon to fix script errors within the sci-fi modpack

---
+++ optionals/scifi/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT scifi
+#define COMPONENT_BEAUTIFIED Sci-Fi
+#include "\x\keko\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_SCIFI
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_SCIFI
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_SCIFI
+#endif
+
+#include "\x\keko\addons\main\script_macros.hpp"
