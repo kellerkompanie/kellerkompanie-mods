8aa64fa47728a3e3c7ceb79b7aa4712f7b309244
Schwaggot <tom.ryan@posteo.de>
2019-03-07 23:04:05 +0100

    closed #47 and adjusted flares based on TBMod

---
+++ addons/common/CfgAmmo.hpp
@@ -0,0 +1,8 @@
+class CfgAmmo {
+    class SmokeShell;
+    class G_40mm_Smoke: SmokeShell {
+        simulation = "shotSmoke";
+        deflectionSlowDown = 0.3;
+        timeToLive = 120;
+    };
+};
