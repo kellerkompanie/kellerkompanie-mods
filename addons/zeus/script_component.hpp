ca5713a7f4c558a051cc84fe640746890242f67b
Schwaggot <tom.ryan@posteo.de>
2019-03-08 19:46:49 +0100

    started working an achilles based zeus extension

---
+++ addons/zeus/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT zeus
+#define COMPONENT_BEAUTIFIED Zeus
+#include "\x\keko\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_ZEUS
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_ZEUS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_ZEUS
+#endif
+
+#include "\x\keko\addons\main\script_macros.hpp"
---
+++ addons/zeus/ui/dialogs/replacement/RscAttributesModules.hpp
@@ -0,0 +1,4 @@
+class RscAttributeCAS : RscControlsGroupNoScrollbars
+{
+    onSetFocus = "[_this,'RscAttributeCAS','AresDisplays'] call (uinamespace getvariable 'keko_zeus_fnc_initCuratorAttribute')";
+};
