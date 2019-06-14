86fa92ae7fd998ebc477cc5e0390de294392d22a
Schwaggot <tom.ryan@posteo.de>
2019-06-14 12:00:50 +0200

    replaced Rsc config since it is considered obsolete now, see https://community.bistudio.com/wiki/Arma_3_Main_Menu#Spotlight

--- addons/button/config.cpp
+++ addons/button/config.cpp
@@ -17,7 +17,7 @@ class CfgPatches {
     };
 };
 
-class RscStandardDisplay;
+/*class RscStandardDisplay;
 class RscDisplayMain: RscStandardDisplay {
     class Spotlight {
         class AwesomeServer {
@@ -31,4 +31,32 @@ class RscDisplayMain: RscStandardDisplay {
             condition = "true"; // Condition for showing the spotlight
         };
     };
+};*/
+
+class CfgMainMenuSpotlight {
+    class AwesomeServer {
+        text = CSTRING(text); // Text displayed on the square button, converted to upper-case
+        textIsQuote = 0; // 1 to add quotation marks around the text
+        picture = QPATHTOF(ui\button.paa); // Square picture, ideally 512x512
+        //video = "\a3\Ui_f\Video\spotlight_1_Apex.ogv"; // Video played on mouse hover
+        //action = "0 = [_this, 'your.domain.name', '2302', 'yourpasshere'] execVM '\amp_spotlight\joinServer.sqf';";
+        action = "0 = [_this, 'server.kellerkompanie.com', '2302', 'keko'] execVM '\x\keko\addons\button\joinServer.sqf';";
+        actionText = CSTRING(actionText); // Text displayed in top left corner of on-hover white frame
+        condition = "true"; // Condition for showing the spotlight
+    };
+    // These are referenced somewhere so they can't be deleted
+    class Orange_Campaign { condition = "false";};
+    class Tacops_Campaign_01 { condition = "false";};
+
+    delete ApexProtocol;
+    delete BootCamp;
+    delete EastWind;
+    delete Orange_CampaignGerman;
+    delete Orange_Showcase_IDAP;
+    delete Orange_Showcase_LoW;
+    delete Showcase_TankDestroyers;
+    delete Tacops_Campaign_02;
+    delete Tacops_Campaign_03;
+    delete Tanks_Campaign_01;
+    delete gm_campaign_01;
 };
