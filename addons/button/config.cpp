9db0bf9f277b4906ec844e9a13d2d962ed16a034
Schwaggot <tom.ryan@posteo.de>
2019-06-23 00:23:53 +0200

    refactoring on button config

--- addons/button/config.cpp
+++ addons/button/config.cpp
@@ -1,4 +1,3 @@
-// Original by Ampersand https://github.com/ampersand38/Server-Spotlight
 #include "script_component.hpp"
 
 class CfgPatches {
@@ -16,22 +15,6 @@ class CfgPatches {
         VERSION_CONFIG;
     };
 };
-
-/*class RscStandardDisplay;
-class RscDisplayMain: RscStandardDisplay {
-    class Spotlight {
-        class AwesomeServer {
-            text = CSTRING(text); // Text displayed on the square button, converted to upper-case
-            textIsQuote = 0; // 1 to add quotation marks around the text
-            picture = QPATHTOF(ui\button.paa); // Square picture, ideally 512x512
-            //video = "\a3\Ui_f\Video\spotlight_1_Apex.ogv"; // Video played on mouse hover
-            //action = "0 = [_this, 'your.domain.name', '2302', 'yourpasshere'] execVM '\amp_spotlight\joinServer.sqf';";
-            action = "0 = [_this, 'server.kellerkompanie.com', '2302', 'keko'] execVM '\x\keko\addons\button\joinServer.sqf';";
-            actionText = CSTRING(actionText); // Text displayed in top left corner of on-hover white frame
-            condition = "true"; // Condition for showing the spotlight
-        };
-    };
-};*/
 
 class CfgMainMenuSpotlight {
     class AwesomeServer {
