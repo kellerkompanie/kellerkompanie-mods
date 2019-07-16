13a5c4353e43d77ac32340180cc52fe6053f336c
Schwaggot <tom.ryan@posteo.de>
2019-07-16 17:00:44 +0200

    added ladder addon

---
+++ addons/ladder/README.md
@@ -0,0 +1,2 @@
+## keko_ladder
+Makes you move up the ladder faster. (Original: https://steamcommunity.com/sharedfiles/filedetails/?id=1804716719)
---
+++ addons/ladder/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT ladder
+#define COMPONENT_BEAUTIFIED Ladder
+#include "\x\keko\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_LADDER
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_LADDER
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_LADDER
+#endif
+
+#include "\x\keko\addons\main\script_macros.hpp"
