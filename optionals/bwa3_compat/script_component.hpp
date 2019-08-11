b3a96f8aaf6aee4b8a17e21e0daee3d66bd62c6e
Schwaggot <tom.ryan@posteo.de>
2019-08-11 16:26:16 +0200

    closed #96

---
+++ optionals/bwa3_compat/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT bwa3_compat
+#define COMPONENT_BEAUTIFIED BWA3 Compat
+#include "\x\keko\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_BWA3_COMPAT
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_BWA3_COMPAT
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_BWA3_COMPAT
+#endif
+
+#include "\x\keko\addons\main\script_macros.hpp"
