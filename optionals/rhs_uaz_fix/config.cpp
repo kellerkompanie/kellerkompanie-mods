6788471314db6446ab57d50cc1ae6af4c14e367c
Schwaggot <tom.ryan@posteo.de>
2019-08-12 22:07:31 +0200

    added fix for current RHS version error related to rhs_uaz_spg9_chdkz

--- optionals/rhs_uaz_fix/config.cpp
+++ optionals/rhs_uaz_fix/config.cpp
@@ -7,7 +7,9 @@ class CfgPatches {
         weapons[] = {};
         requiredVersion= REQUIRED_VERSION;
         requiredAddons[]= {
-            "rhs_main"
+            "rhs_main",
+            "rhs_c_a2port_car",
+            "rhs_cti_insurgents"
         };
         author = ECSTRING(common,Schwaggot);
         authors[] = {"Schwaggot"};
@@ -21,5 +23,9 @@ class CfgVehicles {
     class RHS_UAZ_Base: Offroad_01_base_F {
         forceHideDriver = 0;
         driverInAction = "RHS_UAZ_driver";
+    };
+
+    class RHS_UAZ_SPG9_Base;
+    class rhs_uaz_spg9_chdkz: RHS_UAZ_SPG9_Base {
     };
 };
