0d080eca10ffe1cf09a7ab2032acc1cb295929f9
Schwaggot <tom.ryan@posteo.de>
2019-09-08 00:25:17 +0200

    added increased vest inventory sizes for RHS SPC USMC vests

---
+++ optionals/rhs_vests/CfgWeapons.hpp
@@ -0,0 +1,73 @@
+class CfgWeapons {
+
+    class Vest_Camo_Base;
+    class rhsusf_iotv_ocp_base: Vest_Camo_Base{
+        class ItemInfo;
+    };
+
+    class rhsusf_spc: rhsusf_iotv_ocp_base {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_rifleman: rhsusf_spc {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_corpsman: rhsusf_spc_rifleman {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_crewman: rhsusf_spc_rifleman {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_iar: rhsusf_spc_rifleman {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_light: rhsusf_spc_rifleman {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_mg: rhsusf_spc_rifleman {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_marksman: rhsusf_spc_rifleman {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_patchless: rhsusf_spc_rifleman {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_patchless_radio: rhsusf_spc_rifleman {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_sniper: rhsusf_spc_rifleman {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_squadleader: rhsusf_spc_rifleman {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+    class rhsusf_spc_teamleader: rhsusf_spc_rifleman {
+        class ItemInfo: ItemInfo {
+            containerClass = "Supply140";
+        };
+    };
+};
---
+++ optionals/rhs_vests/README.md
@@ -0,0 +1,2 @@
+## keko_rhs_vests
+Adjusts the size of certain RHS vests to allow simpler loadout creation.
