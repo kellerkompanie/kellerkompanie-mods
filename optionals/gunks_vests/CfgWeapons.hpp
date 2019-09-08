3e1a003a3422aaed84c76a5ad13de9453c985ee9
Schwaggot <tom.ryan@posteo.de>
2019-09-08 19:10:30 +0200

    added first version of gunk's vests

---
+++ optionals/gunks_vests/CfgWeapons.hpp
@@ -0,0 +1,17 @@
+class CfgWeapons {
+
+    class V_PlateCarrier1_blk;
+    class V_PlateCarrier1_wdl: V_PlateCarrier1_blk {
+        hiddenSelectionsTextures[] = {"\A3\Characters_F_Enoch\Vests\Data\vests_wdl_co.paa"};
+    };
+
+    class V_PlateCarrier2_blk;
+    class V_PlateCarrier2_wdl: V_PlateCarrier2_blk {
+        hiddenSelectionsTextures[] = {"\A3\Characters_F_Enoch\Vests\Data\vests_wdl_co.paa"};
+    };
+
+    class V_MU_PlateCarrier2_2_wdl: V_PlateCarrier2_wdl {
+        hiddenSelectionsTextures[] = {"\A3\Characters_F_Enoch\Vests\Data\vests_wdl_co.paa"};
+    };
+
+};
---
+++ optionals/gunks_vests/script_component.hpp
@@ -0,0 +1,17 @@
+#define COMPONENT gunks_vests
+#define COMPONENT_BEAUTIFIED Gunk's Vests
+#include "\x\keko\addons\main\script_mod.hpp"
+
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+
+#ifdef DEBUG_ENABLED_REMOVE_GUNKS_VESTS
+    #define DEBUG_MODE_FULL
+#endif
+
+#ifdef DEBUG_SETTINGS_REMOVE_GUNKS_VESTS
+    #define DEBUG_SETTINGS DEBUG_SETTINGS_REMOVE_GUNKS_VESTS
+#endif
+
+#include "\x\keko\addons\main\script_macros.hpp"
