2bd61df4466cce1233fe6d5e161cc57228c90c01
Schwaggot <tom.ryan@posteo.de>
2019-09-10 17:41:30 +0200

    removed obsolete files of gunk's vests

--- optionals/gunks_vests/CfgWeapons.hpp
+++ optionals/gunks_vests/CfgWeapons.hpp
@@ -1,6 +1,6 @@
 class CfgWeapons {
 
-    class V_PlateCarrier1_blk;
+    /*class V_PlateCarrier1_blk;
     class V_PlateCarrier1_wdl: V_PlateCarrier1_blk {
         hiddenSelectionsTextures[] = {"\A3\Characters_F_Enoch\Vests\Data\vests_wdl_co.paa"};
     };
@@ -12,6 +12,6 @@ class CfgWeapons {
 
     class V_MU_PlateCarrier2_2_wdl: V_PlateCarrier2_wdl {
         hiddenSelectionsTextures[] = {"\A3\Characters_F_Enoch\Vests\Data\vests_wdl_co.paa"};
-    };
+    };*/
 
 };
