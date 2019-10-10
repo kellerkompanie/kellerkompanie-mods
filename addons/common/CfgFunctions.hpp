5bb0b44fe7ceb2936bea0042978393989b6bead2
Schwaggot <tom.ryan@posteo.de>
2019-10-10 22:46:39 +0200

    replaced tabs with whitespaces

--- addons/common/CfgFunctions.hpp
+++ addons/common/CfgFunctions.hpp
@@ -11,9 +11,9 @@ class CfgFunctions {
     class 3denEnhanced {
         tag = "Enh";
         class Garrison {
-			class garrison_garrison {
+            class garrison_garrison {
                 file = QPATHTOF(functions\fnc_garrison.sqf);
             };
-		};
+        };
     };
 };
