14842401a0c812db004e32e6b9cbb771fffd683f
Schwaggot <tom.ryan@posteo.de>
2020-01-01 22:21:53 +0100

    added proper replacement for medical vanilla items for medical rewrite

---
+++ addons/medical/CfgReplacementItems.hpp
@@ -0,0 +1,24 @@
+class ace_medical_replacementItems {
+
+    /* First Aid Kit */
+    ItemType_401[] = {
+        {"ACE_fieldDressing", 15},
+        {"ACE_morphine", 3},
+        {"ACE_epinephrine", 1},
+        {"ACE_tourniquet", 4}
+    };
+
+    /* MediKit */
+    ItemType_619[] = {
+        {"ACE_fieldDressing", 20},
+        {"ACE_elasticBandage", 50},
+        {"ACE_epinephrine", 8},
+        {"ACE_morphine", 8},
+        {"ACE_tourniquet", 6},
+        {"ACE_salineIV", 6},
+        {"ACE_salineIV_500", 6},
+        {"ACE_splint", 10},
+        {"ACE_surgicalKit", 1},
+        {"ACE_personalAidKit", 1}
+    };
+};
---
+++ addons/medical/config.cpp
@@ -0,0 +1,19 @@
+#include "script_component.hpp"
+
+class CfgPatches {
+    class ADDON {
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {
+            "keko_common",
+            "ace_medical_treatment"
+        };
+        author = "Schwaggot";
+        authors[] = {"Schwaggot"};
+        url = ECSTRING(main,URL);
+        VERSION_CONFIG;
+    };
+};
+
+#include "CfgReplacementItems.hpp"
