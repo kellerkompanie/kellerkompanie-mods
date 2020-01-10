ae2950612e9ea5dfa0619072ea3fbc6eec7d55ba
Schwaggot <tom.ryan@posteo.de>
2020-01-10 17:56:00 +0100

    v2.18.0

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -4,9 +4,9 @@
 #define PREFIX keko
 
 #define MAJOR 2
-#define MINOR 17
-#define PATCHLVL 1
-#define BUILD 105
+#define MINOR 18
+#define PATCHLVL 0
+#define BUILD 106
 
 #define VERSION MAJOR.MINOR.PATCHLVL.BUILD
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
