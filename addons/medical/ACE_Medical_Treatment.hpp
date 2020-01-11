1e456dbbb59092edfeb820a643e160865cb1469e
Schwaggot <tom.ryan@posteo.de>
2020-01-11 16:12:43 +0100

    adjusted painkiller pain reduce

--- addons/medical/ACE_Medical_Treatment.hpp
+++ addons/medical/ACE_Medical_Treatment.hpp
@@ -1,8 +1,8 @@
 class ace_medical_treatment {
     class Medication {
         class Painkiller {
-            painReduce = 0.1;
-            timeInSystem = 600;
+            painReduce = 0.4;
+            timeInSystem = 1200;
             timeTillMaxEffect = 60;
             maxDose = 10;
             incompatibleMedication[] = {};
