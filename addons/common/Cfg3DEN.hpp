0b6f406452513d7efb00f69f2801b4a45e774c72
Schwaggot <tom.ryan@posteo.de>
2020-01-24 10:05:46 +0100

    deleted 3DEN attributes option for ACE garrison

--- addons/common/Cfg3DEN.hpp
+++ addons/common/Cfg3DEN.hpp
@@ -3,21 +3,6 @@
 class Cfg3DEN {
     class Object {
         class AttributeCategories {
-            class GVAR(aceGarrison) {
-                collapsed = 1;
-                displayName = CSTRING(3denCategoryACE);
-                class Attributes {
-                    class Enh_Move {
-                        displayName = CSTRING(aceGarrison);
-                        tooltip = CSTRING(aceGarrisonTooltip);
-                        property = "keko_aceGarrison";
-                        control = "Checkbox";
-                        expression = "if(_value) then {_this setVariable ['ace_ai_garrisonned', true]}";
-                        defaultValue = "false";
-                        condition = "objectBrain";
-                    };
-                };
-            };
             class StateSpecial {
                 class Attributes {
                     delete Enh_DoStop; /* delete buggy 3den function */
