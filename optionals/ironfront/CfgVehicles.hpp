965740c09894d3f7be90abcb8ee6c9f9078bdc81
Schwaggot <tom.ryan@posteo.de>
2020-03-03 16:43:17 +0100

    fixed #142

---
+++ optionals/ironfront/CfgVehicles.hpp
@@ -0,0 +1,16 @@
+class CfgVehicles {
+    class B_LIB_AssaultPack_Base;
+    class B_LIB_GER_MedicBackpack: B_LIB_AssaultPack_Base {
+        maximumLoad = 280;
+    };
+
+    class fow_b_tornister;
+    class fow_b_tornister_medic: fow_b_tornister {
+        maximumLoad = 280;
+    };
+
+    class B_LIB_GER_Rucksack_Vide;
+    class B_LIB_GER_Rucksack_Medic: B_LIB_GER_Rucksack_Vide {
+        maximumLoad = 280;
+    };
+};
--- optionals/ironfront/config.cpp
+++ optionals/ironfront/config.cpp
@@ -1,34 +1,40 @@
 #include "script_component.hpp"
 
 class CfgPatches {
-	class ADDON {
-		name = COMPONENT_NAME;
-		units[]= {};
-		weapons[] = {};
-		magazines[] = {
-			QGVAR(LIB_75Rnd_792x57),
-			QGVAR(LIB_150Rnd_792x57),
-			QGVAR(LIB_250Rnd_792x57),
-			QGVAR(FOW_75Rnd_792x57),
-			QGVAR(FOW_150Rnd_792x57),
-			QGVAR(FOW_250Rnd_792x57)
-		};
-		requiredVersion= REQUIRED_VERSION;
-		requiredAddons[]= {
-			"fow_munitions",
-			"fow_weapons",
-			"LIB_weapons",
-			"WW2_Assets_c_Weapons_InfantryWeapons_c",
-			"fow_weapons_c",
-			"cba_jam"
-		};
-		author = ECSTRING(common,Schwaggot);
+    class ADDON {
+        name = COMPONENT_NAME;
+        units[]= {};
+        weapons[] = {};
+        magazines[] = {
+            QGVAR(LIB_75Rnd_792x57),
+            QGVAR(LIB_150Rnd_792x57),
+            QGVAR(LIB_250Rnd_792x57),
+            QGVAR(FOW_75Rnd_792x57),
+            QGVAR(FOW_150Rnd_792x57),
+            QGVAR(FOW_250Rnd_792x57)
+        };
+        requiredVersion= REQUIRED_VERSION;
+        requiredAddons[]= {
+            "fow_munitions",
+            "fow_weapons",
+            "LIB_weapons",
+            "WW2_Assets_c_Weapons_InfantryWeapons_c",
+            "fow_weapons_c",
+            "cba_jam",
+            "WW2_Assets_c_Characters_Germans_c_GER_Wehrmacht_Gear",
+            "WW2_Core_c_WW2_EditorPreviews_c",
+            "fow_characters_c",
+            "GEISTL_EDITORPREVIEWS_C",
+            "GEISTL_ASSETS_C_GER_BAG"
+        };
+        author = ECSTRING(common,Schwaggot);
         authors[] = {"Schwaggot"};
         url = ECSTRING(main,URL);
         VERSION_CONFIG;
-	};
+    };
 };
 
 #include "CfgMagazines.hpp"
 #include "CfgMagazineWells.hpp"
 #include "CfgWeapons.hpp"
+#include "CfgVehicles.hpp"
