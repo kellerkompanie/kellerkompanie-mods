2243ad80d15290be75f824f2dccd7b442187feeb
Schwaggot <tom.ryan@posteo.de>
2020-09-09 10:03:13 +0200

    adjusted blood effects of painkillers

--- addons/medical/ACE_Medical_Treatment.hpp
+++ addons/medical/ACE_Medical_Treatment.hpp
@@ -2,11 +2,14 @@ class ace_medical_treatment {
     class Medication {
         class Painkiller {
             painReduce = 0.4;
+            hrcreaseLow[] = {-5, -10};
+            hrcreaseNormal[] = {-5, -15};
+            hrcreaseHigh[] = {-5, -15};
             timeInSystem = 1200;
             timeTillMaxEffect = 60;
             maxDose = 10;
             incompatibleMedication[] = {};
-            viscosityChange = 5;
+            viscosityChange = -5;
         };
     };
 };
