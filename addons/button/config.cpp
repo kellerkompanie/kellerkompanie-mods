b24233a7dfbd3835fecf8a9e8a08f815272230b0
Schwaggot <tom.ryan@posteo.de>
2020-12-16 17:07:05 +0100

    removed new buttons from main menu

--- addons/button/config.cpp
+++ addons/button/config.cpp
@@ -44,4 +44,7 @@ class CfgMainMenuSpotlight {
     delete gm_campaign_01;
     delete Contact_Campaign;
     delete OldMan;
+    delete Tacops_Campaign_01;
+    delete SP_FD14;
+    delete Orange_Campaign;
 };
